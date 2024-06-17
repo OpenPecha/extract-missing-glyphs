1aafc0e4abb5d948dd9ee0ea538b54b7a40cdfdf
tenkal <tenkalden007@gmail.com>
2024-06-17 10:15:23 +0530

    updated scripts

--- src/extract-missing-glyph/extract_subjoined_glyphs.py
+++ src/extract-missing-glyph/extract_subjoined_glyphs.py
@@ -1,27 +1,41 @@
-import codecs
+import os
+import shutil
+import re
 
-def read_file(file_path):
-    with codecs.open(file_path, 'r', encoding='utf-8') as file:
-        return file.read()
+def tibetan_char_to_codepoint(char):
+    return ord(char)
 
-def write_file(file_path, content):
-    with codecs.open(file_path, 'w', encoding='utf-8') as file:
-        file.write(content)
+def read_tibetan_char_from_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        characters = f.read()
+    return [ord(char) for char in characters]
 
-def find_common_characters(text1, text2, output_file_path):
-    unicode_text1 = [ord(char) for line in text1.splitlines() for char in line.strip()]
-    unicode_text2 = [ord(char) for line in text2.splitlines() for char in line.strip()]
-
-    common_unicode = set(unicode_text2).intersection(set(unicode_text1))
-    common_characters = ''.join([chr(code) for code in common_unicode])
-
-    with codecs.open(output_file_path, 'w', encoding='utf-8') as output_file:
-        output_file.write(common_characters)
+def copy_matching_images(source_dir, target_dir, tibetan_char_codepoints):
+    tibetan_char_pattern = re.compile(r'[\u0F00-\u0FFF]')
+    
+    for filename in os.listdir(source_dir):
+        matches = tibetan_char_pattern.findall(filename)
+        if matches:
+            for char in matches:
+                if tibetan_char_to_codepoint(char) in tibetan_char_codepoints:
+                    subdirectory = os.path.join(target_dir, char)
+                    if not os.path.exists(subdirectory):
+                        os.makedirs(subdirectory)
+                    
+                    source_file = os.path.join(source_dir, filename)
+                    target_file = os.path.join(subdirectory, filename)
+                    shutil.copy2(source_file, target_file)
+                    print(f"copied {filename} to {subdirectory}")
+                    break
 
 def main():
-    found_glyphs = read_file('../../data/derge_found_glyphs.txt')
-    subjoined_glyphs = read_file('../../data/subjoined_glyphs.txt')
-    find_common_characters(found_glyphs, subjoined_glyphs, '../../data/common_glyphs.txt')
+    source_directory = r"C:\Users\tenka\monlam\project\create-font-from-glyph\data\font_data\derge_font\Derge_test_ten_glyphs\downloaded_images"
+    target_directory = "../../data/subjoined_glyphs/derge"
+    text_file_path = "../../data/subjoined_glyphs.txt"
 
+    tibetan_codepoints = read_tibetan_char_from_file(text_file_path)
+    copy_matching_images(source_directory, target_directory, tibetan_codepoints)
+    
 if __name__ == "__main__":
     main()
+    
