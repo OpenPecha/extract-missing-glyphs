a5d50daa57bfc370e7bd2a0c2b6e8cbd9a45d59d
tenkal <tenkalden007@gmail.com>
2024-06-28 10:26:05 +0530

    updated gitgnore

--- src/extract-missing-glyph/apply_ocr_to_image.py
+++ src/extract-missing-glyph/apply_ocr_to_image.py
@@ -7,6 +7,7 @@ import os
 os.environ['TESSDATA_PREFIX'] = "C:\\Users\\tenka\\tessdata"
 
 def extract_tibetan_text_indices(image_path, output_path):
+    image_name = os.path.basename(image_path)
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     custom_config = r'--oem 3 --psm 6 -l bod'
@@ -14,8 +15,10 @@ def extract_tibetan_text_indices(image_path, output_path):
 
     extracted_data = []
     n_boxes = len(data['level'])
+    unique_index = 0
+
     for i in range(n_boxes):
-        if int(data['conf'][i]) > 0: 
+        if int(data['conf'][i]) > 0:
             word = data['text'][i]
             word_box = {
                 'left': data['left'][i],
@@ -23,31 +26,48 @@ def extract_tibetan_text_indices(image_path, output_path):
                 'width': data['width'][i],
                 'height': data['height'][i]
             }
-            word_confidence = data['conf'][i]
 
-            if word:  
+            if word.strip(): 
                 char_width = word_box['width'] / len(word)
                 for j, char in enumerate(word):
                     char_info = {
+                        'image_name': image_name,
                         'text': char,
-                        'confidence': word_confidence,
                         'bounding_box': {
                             'left': int(word_box['left'] + j * char_width),
                             'top': word_box['top'],
                             'width': int(char_width),
                             'height': word_box['height']
                         },
-                        'index': i
+                        'index': unique_index
                     }
                     extracted_data.append(char_info)
+                    unique_index += 1
 
-    json_data = json.dumps(extracted_data, ensure_ascii=False, indent=4)
+                if i < n_boxes - 1 and data['text'][i + 1].strip():
+                    space_width = 10 
+                    char_info = {
+                        'image_name': image_name,
+                        'text': ' ',
+                        'bounding_box': {
+                            'left': int(word_box['left'] + word_box['width']),
+                            'top': word_box['top'],
+                            'width': space_width,
+                            'height': word_box['height']
+                        },
+                        'index': unique_index
+                    }
+                    extracted_data.append(char_info)
+                    unique_index += 1
+
     with open(output_path, 'w', encoding='utf-8') as f:
-        f.write(json_data)
+        for entry in extracted_data:
+            json.dump(entry, f, ensure_ascii=False)
+            f.write('\n')
 
-    print(f"json saved at {output_path}")
+    print(f"JSONL saved at {output_path}")
 
-image_path = '../../data/source_images/W2KG209989/I2KG2100520029.jpg'
-output_path = '../../data/index_json/extracted_text_indices.json'
+image_path = '../../data/source_images/W22084/08860005.tif'
+output_path = '../../data/ocr_jsonl/extracted_text_indices.jsonl'
 
 extract_tibetan_text_indices(image_path, output_path)
--- src/extract-missing-glyph/pipeline_opf_glyph_extraction.py
+++ src/extract-missing-glyph/pipeline_opf_glyph_extraction.py
@@ -2,21 +2,17 @@ import yaml
 import json
 from pathlib import Path
 
-
 def read_text(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         return file.read()
-
 
 def read_char(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         return file.readlines()
 
-
 def load_yaml(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         return yaml.safe_load(file)
-
 
 def find_spans(text, characters):
     spans = {char: [] for char in characters}
@@ -26,7 +22,6 @@ def find_spans(text, characters):
             spans[char].append(index)
             index = text.find(char, index + 1)
     return spans
-
 
 def check_spans_against_yaml(spans, yaml_data, base_index=0):
     references = {char: [] for char in spans.keys()}
@@ -46,7 +41,6 @@ def check_spans_against_yaml(spans, yaml_data, base_index=0):
 def read_missing_glyphs(file_path):
     characters = read_char(file_path)
     return [char.strip() for char in characters]
-
 
 def find_and_check_spans(base_dir, layers_dir, characters, meta_data):
     all_spans = []
@@ -83,11 +77,53 @@ def find_and_check_spans(base_dir, layers_dir, characters, meta_data):
 
     return all_spans
 
+def find_relative_spans(base_dir, layers_dir, characters, meta_data):
+    img_span_data = []
+    text_files = sorted(base_dir.glob('*.txt'))
+    yaml_dirs = sorted(layers_dir.glob('*/'))
+
+    for txt_file, yml_dir in zip(text_files, yaml_dirs):
+        opf_text = read_text(txt_file)
+        spans = find_spans(opf_text, characters)
+        base_file_name = txt_file.name
+        image_group_id = None
+        for volume in meta_data['source_metadata']['volumes'].values():
+            if volume['base_file'] == base_file_name:
+                image_group_id = volume['image_group_id']
+                break
+
+        for yml_file in yml_dir.glob('*.yml'):
+            if yml_file.name == 'Pagination.yml':
+                yaml_data = load_yaml(yml_file)
+                for annotation in yaml_data['annotations'].values():
+                    start = annotation['span']['start']
+                    end = annotation['span']['end']
+                    reference = annotation['reference']
+                    relative_spans = {}
+                    for char in characters:
+                        relative_spans[char] = []
+                    for char, span_list in spans.items():
+                        for span in span_list:
+                            if start <= span < end:
+                                relative_span = span - start
+                                relative_spans[char].append(relative_span)
+                    for char, rel_span_list in relative_spans.items():
+                        if rel_span_list:
+                            img_span_data.append({
+                                "char": char,
+                                "txt_file": txt_file.name,
+                                "reference": {reference: rel_span_list}
+                            })
+
+    return img_span_data
 
 def save_to_json(span_data, filename):
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(span_data, file, ensure_ascii=False, indent=4)
 
+def save_img_span_to_json(img_span_data, filename):
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(img_span_data, file, ensure_ascii=False, indent=4)
 
 def main():
     base_dir = Path('../../data/opf/P000001.opf/base/')
@@ -102,6 +138,9 @@ def main():
     span_path = "../../data/span/span.json"
     save_to_json(all_spans, span_path)
 
+    img_span_data = find_relative_spans(base_dir, layers_dir, characters, meta_data)
+    img_span_path = "../../data/span/img_span.json"
+    save_img_span_to_json(img_span_data, img_span_path)
 
 if __name__ == "__main__":
     main()
